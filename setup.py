7adce17ffce38bef11fc3f19cc6a32db17ea1398
Pof Magicfingers <pof@pof.pm>
2014-05-11 00:48:48 +0200

    Updated README and setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
 	name="mclogalyzer",
-	version="1.0",
+	version="1.1",
 	description="Minecraft Server Log Analyzer",
 	author="Moritz Hilscher",
 	author_email="moritz.hilscher@gmail.com",
