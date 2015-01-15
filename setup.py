c2684d4a7916aa32f36aab7a1e7fe5e0053577bd
woltman <john@johnwoltman.com>
2015-01-15 13:55:24 -0500

    PEP8: convert tabs to spaces

--- setup.py
+++ setup.py
@@ -2,15 +2,16 @@
 
 from distutils.core import setup
 
+
 setup(
-	name="mclogalyzer",
-	version="1.1",
-	description="Minecraft Server Log Analyzer",
-	author="Moritz Hilscher",
-	author_email="moritz.hilscher@gmail.com",
-	license="GNU General Public License v3",
-	url="http://github.com/m0r13/mclogalyzer",
-	packages=["mclogalyzer"],
-	package_data={"mclogalyzer" : ["template.html"]},
-	scripts=["bin/mclogalyzer"],
+    name="mclogalyzer",
+    version="1.1",
+    description="Minecraft Server Log Analyzer",
+    author="Moritz Hilscher",
+    author_email="moritz.hilscher@gmail.com",
+    license="GNU General Public License v3",
+    url="http://github.com/m0r13/mclogalyzer",
+    packages=["mclogalyzer"],
+    package_data={"mclogalyzer": ["template.html"]},
+    scripts=["bin/mclogalyzer"],
 )
